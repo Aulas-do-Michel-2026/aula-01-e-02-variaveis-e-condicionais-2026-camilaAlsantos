efb4afeba6bc5b9c32c8e9989040a6c52a3aa183
camilaAlsantos <camilaalmeidasantoscons@gmail.com>
2026-04-25 18:12:26 -0300

    Update exercicio_5.py

--- exercicio_5.py
+++ exercicio_5.py
@@ -1,4 +1,3 @@
-"""
 #### Exercício 3 - Identificar se a variante está no gene BRCA1 - Versão 2.
 
 Receba 3 inputs do usuário:
@@ -54,4 +53,17 @@ Digite o genoma de referência: hg38
 Resposta:
 Não
 
-"""
+
+cromossomo = input("Digite o cromossomo: ")
+posicao = int(input("Digite a posição: "))
+genoma = input("Digite o genoma de referência: ")
+
+if cromossomo == "chr17":
+    if genoma == "hg19" and 41196312 <= posicao <= 41277500:
+        print("Sim")
+    elif genoma == "hg38" and 43044295 <= posicao <= 43125483:
+        print("Sim")
+    else:
+        print("Não")
+else:
+    print("Não")
